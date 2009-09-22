365560a337eb0436628f367117822eb42dcdf9c5
Josh <jgraham@jagair.corporate.thoughtworks.com>
2009-09-22 16:29:12 +0530

    JoshG - tests for broader characters, escaping double-quotes and backslashes, static convenience method

--- src/com/thoughtworks/quoter/Quoter.java
+++ src/com/thoughtworks/quoter/Quoter.java
@@ -8,11 +8,31 @@ public class Quoter {
 	}
 
 	private String quote(String string) {
-		return string;
+		if (string == null) return string;
+
+		StringBuffer buffer = new StringBuffer(string.length() + 16);
+		for (int i = 0; i < string.length(); i++) {
+			char ch = string.charAt(i);
+			if (ch == '"' || ch == '\\') buffer.append('\\');
+			buffer.append(ch);
+		}
+
+		return buffer.toString();
 	}
 
 	public String quoted() {
 		return processed;
 	}
 
+	/**
+	 * Static convenience method. Identical to: <br/>
+	 * <code>new Quoter(string).quoted();</code>
+	 * 
+	 * @param string
+	 *          the string to quote
+	 * @return the string with double-quotes and backslashes escaped
+	 */
+	public static String quoted(String string) {
+		return new Quoter(string).quoted();
+	}
 }
--- test/com/thoughtworks/quoter/QuoterTest.java
+++ test/com/thoughtworks/quoter/QuoterTest.java
@@ -1,19 +1,57 @@
 package com.thoughtworks.quoter;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class QuoterTest {
 	@Test
-	public void shouldReturnEmptyStringWhenGivenEmptyString() throws Exception {
-		Quoter quoter = new Quoter("");
-		assertEquals("", quoter.quoted());
+	public void shouldReturnNullWhenGivenNull() throws Exception {
+		assertNull(new Quoter(null).quoted());
+		assertNull(Quoter.quoted(null));
 	}
 
 	@Test
-	public void shouldReturnNullWhenGivenNull() throws Exception {
-		Quoter quoter = new Quoter(null);
-		assertNull(quoter.quoted());
+	public void shouldReturnEmptyStringWhenGivenEmptyString() throws Exception {
+		assertEquals("", new Quoter("").quoted());
+		assertEquals("", Quoter.quoted(""));
+	}
+
+	@Test
+	public void shouldReturnSameWhenGivenOnlyAlphaNumChars() throws Exception {
+		String expected = "ABCXYZabcxyz0123";
+		assertEquals(expected, new Quoter(expected).quoted());
+		assertEquals(expected, Quoter.quoted(expected));
+	}
+
+	@Test
+	public void shouldReturnSameWhenGivenOnlyNonSpecialChars() throws Exception {
+		String expected = "`~!@#$%^&*()_+-= {}[]|;:<>,.?/";
+		assertEquals(expected, new Quoter(expected).quoted());
+		assertEquals(expected, Quoter.quoted(expected));
+	}
+
+	@Test
+	public void shouldReturnSameWhenGivenDiacriticalChars() throws Exception {
+		String expected = "¡ªºŭảẖ˝āœˀ";
+		assertEquals(expected, new Quoter(expected).quoted());
+		assertEquals(expected, Quoter.quoted(expected));
+	}
+
+	@Test
+	public void shouldReturnSlashDQuoteWhenGivenDQuote() throws Exception {
+		assertEquals("\\\"", new Quoter("\"").quoted());
+		assertEquals("\\\"", Quoter.quoted("\""));
+	}
+
+	@Test
+	public void shouldReturnSQuoteWhenGivenSQuote() throws Exception {
+		assertEquals("'", new Quoter("'").quoted());
+		assertEquals("'", Quoter.quoted("'"));
+	}
+
+	@Test
+	public void shouldReturnSlashSlashWhenGivenSlash() throws Exception {
+		assertEquals("\\\\", new Quoter("\\").quoted());
+		assertEquals("\\\\", Quoter.quoted("\\"));
 	}
 }
