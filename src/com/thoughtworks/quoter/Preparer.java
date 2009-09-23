8ffbb4b49160b8d6a5eebefefd5cb1750557ae93
Josh <jgraham@jagair.corporate.thoughtworks.com>
2009-09-23 16:01:08 +0530

    JoshG - allow any objects as arguments. only double-quote strings. handle nulls better

--- src/com/thoughtworks/quoter/Preparer.java
+++ src/com/thoughtworks/quoter/Preparer.java
@@ -3,18 +3,37 @@ package com.thoughtworks.quoter;
 public class Preparer {
 	private final String prepared;
 
-	public Preparer(final String template, final String... strings) {
+	public Preparer(final String template, final Object... strings) {
 		this.prepared = prepare(template, strings);
 	}
 
-	private String prepare(final String template, final String... arguments) {
+	private String prepare(final String template, final Object... arguments) {
+		if (template == null) return template;
+		if (arguments == null) return template;
+
 		String result = template;
-		for (String arg : arguments) {
-			Quoter quoter = new Quoter(arg);
-			String replace = quoter.quoted();
+		for (Object arg : arguments) {
+			if (arg == null) continue;
+
+			String replace;
+			if (arg instanceof String) replace = Quoter.quoted((String) arg); // only surround strings with double-quotes
+			else replace = arg.toString();
+
 			result = result.replaceFirst("(?<!\\\\)\\?", replace); // replace ? but not \?
 		}
 		return result;
+	}
+
+	/**
+	 * Static convenience method. Identical to: <br/>
+	 * <code>new Preparer(template, arguments).prepared();</code>
+	 * 
+	 * @param string
+	 *          the string to
+	 * @return the string with double-quotes and backslashes escaped
+	 */
+	public static String prepared(final String template, final Object... arguments) {
+		return new Preparer(template, arguments).prepared();
 	}
 
 	public String prepared() {
--- test/com/thoughtworks/quoter/PreparerTest.java
+++ test/com/thoughtworks/quoter/PreparerTest.java
@@ -5,34 +5,55 @@ import org.junit.Test;
 
 public class PreparerTest {
 	@Test
-	public void shouldReturnTemplateWhenNoTokens() throws Exception {
-		String template = "Hello";
-		assertEquals(template, new Preparer(template, (String) null).prepared());
+	public void shouldReturnNullWhenTemplateNull() throws Exception {
+		assertEquals(null, new Preparer(null).prepared());
 	}
 
 	@Test
-	public void shouldReplaceTokenWithString() throws Exception {
+	public void shouldReturnTemplateWhenNoArgs() throws Exception {
+		assertEquals("", new Preparer("").prepared());
+	}
+
+	@Test
+	public void shouldReturnTemplateWhenTokensButNoArgs() throws Exception {
+		String template = "Hello ?";
+		assertEquals(template, new Preparer(template).prepared());
+	}
+
+	@Test
+	public void shouldReturnTemplateWhenNoTokens() throws Exception {
+		String template = "Hello";
+		assertEquals(template, new Preparer(template, (Object) null).prepared());
+	}
+
+	@Test
+	public void shouldReplaceTokenWithQuotedString() throws Exception {
 		String expected = "Hello \"stranger\"!";
 		String template = "Hello ?!";
 		assertEquals(expected, new Preparer(template, "stranger").prepared());
 	}
 
 	@Test
-	public void shouldReplaceTwoTokensWithStrings() throws Exception {
+	public void shouldReplaceTwoTokensWithQuotedStrings() throws Exception {
 		String expected = "Howdy \"partner\", it's \"cold\" here!";
 		String template = "Howdy ?, it's ? here!";
 		assertEquals(expected, new Preparer(template, "partner", "cold").prepared());
 	}
 
 	@Test
-	public void shouldNotReplaceEscapedTokenWithString() throws Exception {
+	public void shouldReplaceTokenWithUnquotedInteger() throws Exception {
+		assertEquals("Hi-5", Preparer.prepared("Hi-?", 5));
+	}
+
+	@Test
+	public void shouldNotReplaceEscapedToken() throws Exception {
 		String expected = "Hello \\?!";
 		String template = "Hello \\?!";
 		assertEquals(expected, new Preparer(template, "stranger").prepared());
 	}
 
 	@Test
-	public void shouldNotReplaceEscapedTokenButReplaceTokenWithString() throws Exception {
+	public void shouldNotReplaceEscapedTokenButReplaceTokenWithQuotedString() throws Exception {
 		String expected = "Hello \\? \"stranger\"!";
 		String template = "Hello \\? ?!";
 		assertEquals(expected, new Preparer(template, "stranger").prepared());
